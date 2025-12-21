302e867037920b2257c8cffed0b48e61e9bc72db
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2025-12-21 14:37:38 +0530

    Time: 45 ms (36.15%) | Memory: 47.8 MB (31.99%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,8 +2,34 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        Boolean dp[][] = new Boolean[n + 1][m + 1];
-        return solve(s.length(), p.length(), s, p, dp);
+        boolean dp[][] = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for(int j = 1; j < m + 1; j++){
+            boolean check = false;
+            for(int k = j; k > 0; k--){
+                if(p.charAt(k - 1) != '*'){
+                    check = true;
+                }
+            }
+            if(check == true){
+                dp[0][j] = false;
+            }else{
+                dp[0][j] = true;
+            }
+        }
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < m + 1; j++){
+                int ch = p.charAt(j - 1);
+                if(s.charAt(i - 1) == ch || ch == '?'){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else if(ch == '*'){
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
     }
     public boolean solve(int i, int j, String s, String p, Boolean dp[][]){
         if(j == 0){
