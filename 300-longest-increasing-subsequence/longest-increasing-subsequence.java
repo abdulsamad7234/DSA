e9e91eac869f291f8795bd341613e1a75d8bc0a4
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2025-12-21 18:46:01 +0530

    Time: 77 ms (22.92%) | Memory: 169.3 MB (17.04%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,32 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        Integer dp[][] = new Integer[n + 1][n + 2];
-        return solve(nums.length, -1, nums, dp);
+        Set<Integer> set = new HashSet<>();
+        for(int ele : nums){
+            set.add(ele);
+        }
+        int arr2[] = new int[set.size()];
+        int idx = 0;
+        for(int ele : set){
+            arr2[idx++] = ele;
+        }
+        Arrays.sort(arr2);
+
+        return solve(nums, arr2);
     }
 
-    public int solve(int i, int prev, int nums[], Integer dp[][]){
-        if(i == 0){
-            return 0;
+    int solve(int arr1[], int arr2[]){
+        int n = arr1.length;
+        int m = arr2.length;
+        int dp[][] = new int[n + 1][m + 1];
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < m + 1; j++){
+                if(arr1[i - 1] == arr2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j] , dp[i][j - 1]);
+                }
+            }
         }
-        if(dp[i][prev + 1] != null){
-            return dp[i][prev + 1];
-        }
-        int notTake = solve(i - 1, prev, nums, dp);
-        int take = 0;
-        if(prev == -1 || nums[prev] > nums[i - 1]){
-            take = 1 + solve(i - 1, i - 1, nums, dp);
-        }
-        return dp[i][prev + 1] = Math.max(notTake, take);
+        return dp[n][m];
     }
 }
