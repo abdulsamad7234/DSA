bc9e9d819aa15afcfa8a818d74e6af2ceebc8fc4
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2025-12-22 11:58:24 +0530

    Time: 47 ms (37.67%) | Memory: 46.2 MB (45.14%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,32 +1,19 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int ele : nums){
-            set.add(ele);
+        int n = nums.length;
+        int dp[] = new int[n];
+        for(int i = 0; i < n; i++){
+            dp[i] = 1;
         }
-        int arr2[] = new int[set.size()];
-        int idx = 0;
-        for(int ele : set){
-            arr2[idx++] = ele;
-        }
-        Arrays.sort(arr2);
-
-        return solve(nums, arr2);
-    }
-
-    int solve(int arr1[], int arr2[]){
-        int n = arr1.length;
-        int m = arr2.length;
-        int dp[][] = new int[n + 1][m + 1];
-        for(int i = 1; i < n + 1; i++){
-            for(int j = 1; j < m + 1; j++){
-                if(arr1[i - 1] == arr2[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }else{
-                    dp[i][j] = Math.max(dp[i - 1][j] , dp[i][j - 1]);
+        int max = 1;
+        for(int curr = 1; curr < n; curr++){
+            for(int prev = 0; prev < curr; prev++){
+                if(nums[prev] < nums[curr] && dp[prev] + 1 > dp[curr]){
+                    dp[curr] = dp[prev] + 1;
+                    max = Math.max(max, dp[curr]); 
                 }
             }
         }
-        return dp[n][m];
+        return max;
     }
 }
